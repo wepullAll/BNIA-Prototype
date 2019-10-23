d219c6e19bf89ccdd6d37beea7622c726941e9a7
Keira <keiraskenandore@gmail.com>
2019-10-22 19:02:28 -0500

    attempted navigation using javascript

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,4 +1,20 @@
 function BackButton(e) {
     e.preventDefault();
     window.history.back();
-});
+};
+
+function togglePage(id) {
+    nav = ['chart', 'trends', 'data', 'map'];
+    navLinks = ['chart-nav', 'trends-nav', 'data-nav', 'map-nav'];
+    page = document.getElementById(id);
+    active = id + '-nav';
+
+    var i;
+    for (i = 0; i < nav.length; i++) {
+        document.getElementById(nav[i]).style.display = "none";
+        document.getElementById(navLinks[i]).classList.remove("active");
+      }
+
+    document.getElementById(active).classList.add("active");
+    page.style.display = "block";
+}
